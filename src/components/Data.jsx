a33b32d41731d7c9be376c6b6f891129287a9571
Simon <cema2077@hotmail.com>
2024-12-12 14:33:21 +0200

    First Commit

---
+++ src/components/Data.jsx
@@ -0,0 +1,14 @@
+import Form from "./Form.jsx";
+import Weather from "./Weather.jsx";
+
+const Data = () => {
+
+    return (
+        <div>
+            <Form/>
+            <Weather/>
+        </div>
+    );
+};
+
+export default Data;
---
+++ src/configureStore/store.js
@@ -0,0 +1,4 @@
+import {legacy_createStore as createStore} from "redux";
+import {weatherReducer} from "../reducers/weatherReducer.js";
+
+export const store = createStore(weatherReducer);
